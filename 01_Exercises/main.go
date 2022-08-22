ead0da61d4ef0181a78ec651fae1aac229edf1ac
gio <56406617+mikoBerries@users.noreply.github.com>
2022-08-22 15:52:24 +0700

    exercises 2

--- 01_Exercises/main.go
+++ 01_Exercises/main.go
@@ -9,7 +9,30 @@ func main() {
 }
 
 func HandsOnExcise2() {
+	/*
+		Hands-on exercise #2
+		1. Use var to DECLARE three VARIABLES. The variables should have package level
+		scope. Do not assign VALUES to the variables. Use the following IDENTIFIERS for the
+		variables and make sure the variables are of the following TYPE (meaning they can
+		store VALUES of that TYPE).
+			a. identifier “x” type int
+			b. identifier “y” type string
+			c. identifier “z” type bool
+		2. in func main
+			a. print out the values for each identifier
+			b. The compiler assigned values to the variables. What are these values called?
+	*/
 	fmt.Println("HandsOnExcise2 ->Start")
+
+	var x int
+	var y string
+	var z bool
+	fmt.Print(x)
+	fmt.Printf("\t %T\n", x)
+	fmt.Print(y)
+	fmt.Printf("\t %T\n", y)
+	fmt.Print(z)
+	fmt.Printf("\t %T\n", z)
 
 	fmt.Println("HandsOnExcise2 ->END")
 }
