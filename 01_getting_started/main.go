671cb5fe3d3664dd52cbc8595ba63bbef81fcbf7
gio <56406617+mikoBerries@users.noreply.github.com>
2022-08-22 08:54:24 +0700

    01_getting_Started

---
+++ 01_getting_started/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("123") //1 line
+
+	fmt.Printf("123\n") //with costume format
+	fmt.Printf("%d - %b \n", 42, 42)
+
+	fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
+	// 42 (decimal)   101010 (binary)    0X2A(Hexa)
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+
+	fmt.Print("123")
+
+	//looping
+	for i := 0; i <= 100; i++ {
+		fmt.Println(i)
+	}
+}
