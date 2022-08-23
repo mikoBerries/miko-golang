a26febdd89946d83ead556c6c32db45ae751a48e
gio <56406617+mikoBerries@users.noreply.github.com>
2022-08-23 23:20:48 +0700

    exercise 5

--- 01_Exercises/main.go
+++ 01_Exercises/main.go
@@ -1,12 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	HandsOnExcise1()
 	HandsOnExcise2()
 	HandsOnExcise3()
 	HandsOnExcise4()
+	HandsOnExcise5()
+}
+
+func HandsOnExcise5() {
+	/*
+		1. at the package level scope, using the “var” keyword, create a VARIABLE with the
+			IDENTIFIER “y”. The variable should be of the UNDERLYING TYPE of your custom
+			TYPE “x”
+		2. n func main
+			a. this should already be done
+				i. print out the value of the variable “x”
+				ii. print out the type of the variable “x”
+				iii. assign your own VALUE to the VARIABLE “x” using the “=” OPERATOR
+				iv. print out the value of the variable “x”
+			b. now do this
+				1.now use CONVERSION to convert the TYPE of the VALUE stored in “x”
+				to the UNDERLYING TYPE
+					1. then use the “=” operator to ASSIGN that value to “y”
+					2. print out the value stored in “y”
+					3. print out the type of “y”
+	*/
+	type hotdog int
+	var x hotdog
+	var y int
+	x = 42
+	y = int(x)
+
+	fmt.Printf("%v \t %T \n", y, y) //still int
+	fmt.Printf("%v \t %T \n", x, x)
 }
 
 func HandsOnExcise4() {
