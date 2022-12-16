5c5ba46b28ae740c7ed4ae0063941ffb6145fc06
gio <56406617+mikoBerries@users.noreply.github.com>
2022-12-16 18:27:06 +0700

    12_update

---
+++ 12_Writing_Documentation/main.go
@@ -0,0 +1,17 @@
+/*
+	printfunc this is a doc foR this file
+	abcdefgh ....
+*/
+
+package printfunc
+
+//to call prints package documentation for the package in the current director
+import "fmt"
+
+//func to print hello world
+// to call go doc <package.func name>
+func Firstfunction() {
+	fmt.Println("hello world")
+}
+
+// if documentation is too big use separate file doc.go
